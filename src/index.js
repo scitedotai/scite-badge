1b217e5f3579b8daac86a72051efed608f06870d
Milo Mordaunt <milomord@gmail.com>
2019-07-23 15:26:10 -0400

    [hotfix] Make sure to insert badges after document loaded

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import { Tally } from 'scite-extension'
 import 'scite-extension/src/styles.css'
 import './styles.css'
 
-const badges = document.querySelectorAll('.scite-badge')
-
-for (let badge of badges) {
-  const data = badge.dataset
-  render(<Tally doi={data.doi} />, badge)
+function insertBadges () {
+  const badges = document.querySelectorAll('.scite-badge')
+  for (let badge of badges) {
+    const data = badge.dataset
+    render(<Tally doi={data.doi} />, badge)
+  }
 }
+
+window.addEventListener('load', () => insertBadges())
