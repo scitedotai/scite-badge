0a11ed465b07627e7c8ce2f41d107c1bdc6151d3
Milo Mordaunt <milomord@gmail.com>
2019-09-26 10:51:42 -0400

    [fix scitedotai/scite#561] Push latest version as one filename also

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,13 +7,28 @@ VERSION=$(git describe --always)
 # Uploads file from ./dist to blob storage
 # $1 = filename
 function upload_blob() {
-    az storage blob upload \
-       --account-name $ACCOUNT \
-       -c $CONTAINER \
-       --name $1 \
-       -f dist/$1 \
-       --content-cache-control $CACHE_CONTROL
+    if [ $# -eq 1 ]
+    then
+        az storage blob upload \
+           --account-name $ACCOUNT \
+           -c $CONTAINER \
+           --name $1 \
+           -f dist/$1 \
+           --content-cache-control $CACHE_CONTROL
+    elif [ $# -eq 2 ]
+    then
+        az storage blob upload \
+           --account-name $ACCOUNT \
+           -c $CONTAINER \
+           --name $2\
+           -f dist/$1 \
+           --content-cache-control $CACHE_CONTROL
+
+    fi
 }
 
 upload_blob "scite-badge-$VERSION.min.js"
 upload_blob "scite-badge-$VERSION.min.css"
+
+upload_blob "scite-badge-$VERSION.min.js" "scite-badge-latest.min.js"
+upload_blob "scite-badge-$VERSION.min.css" "scite-badge-latest.min.css"
