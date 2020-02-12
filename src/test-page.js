ccf7efe29b7321d01fa523696d04c423e61d06c3
Milo Mordaunt <milomord@gmail.com>
2020-02-12 14:39:17 -0500

    [scitedotai/scite#834] Better test page

--- src/test-page.js
+++ src/test-page.js
@@ -4,39 +4,31 @@ import { render } from 'react-dom'
 const rows = [
   {
     doi: '10.1891/0889-8391.13.2.158',
-    layout: 'horizontal'
-  },
-  {
-    doi: '10.1891/0889-8391.13.2.158',
-    layout: 'vertical'
+    layout: 'vertical',
+    showLabels: true
   },
   {
     doi: '10.notrealjoaifds',
     layout: 'vertical'
   },
   {
-    doi: '10.1016/j.biopsych.2005.08.012',
-    layout: 'vertical'
+    doi: '10.1891/0889-8391.13.2.158',
+    layout: 'horizontal',
+    showLabels: true
+  },
+  {
+    doi: '10.1891/0889-8391.13.2.158',
+    layout: 'horizontal',
+    showLabels: false
   },
   {
     doi: '10.1016/j.biopsych.2005.08.012',
-    layout: 'vertical'
-  },
-  {
-    doi: '10.1016/j.biopsych.2005.08.012',
-    layout: 'vertical'
-  },
-  {
-    doi: '10.1016/j.biopsych.2005.08.012',
-    layout: 'vertical'
-  },
-  {
-    doi: '10.1016/j.biopsych.2005.08.012',
-    layout: 'vertical'
+    layout: 'vertical',
+    showLabels: false
   }
 ]
 
-const Row = ({ doi, layout }) => (
+const Row = ({ doi, layout, showLabels }) => (
   <div className='badge-row'>
     <p className='paper'>
       etsi vereor, iudices, ne turpe sit pro fortissimo viro dicere incipientem timere minimeque deceat, cum T. Annius ipse magis de rei publicae salute quam de sua perturbetur1, me ad eius causam parem animi magnitudinem adferre non posse, tamen haec novi iudici nova forma terret oculos qui, quocumque inciderunt, veterem consuetudinem fori et pristinum morem iudiciorum requirunt.
@@ -45,16 +37,24 @@ const Row = ({ doi, layout }) => (
       className='scite-badge'
       data-doi={doi}
       data-layout={layout}
-      data-show-labels='true'
+      data-show-labels={String(!!showLabels)}
     />
   </div>
 )
 
 const App = () => (
-  <div className='badges'>
-    {rows.map(({ doi, layout }, id) => (
-      <Row key={id} doi={doi} layout={layout} />
-    ))}
+  <div>
+    <div className='badges'>
+      {
+        rows.map(({ doi, layout, showLabels }, id) => (
+          <Row key={id} doi={doi} layout={layout} showLabels={showLabels} />
+        ))
+      }
+    </div>
+
+    <div className='narrow'>
+      <Row doi='10.1016/j.biopsych.2005.08.012' layout='vertical' />
+    </div>
   </div>
 )
 
