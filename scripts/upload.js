ed0d093db716178b639ab79a2b107ec0d42bba8a
Milo Mordaunt <milomord@gmail.com>
2022-04-01 14:50:06 -0400

    [scitedotai/scite#4270] Update upload script

--- scripts/upload.js
+++ scripts/upload.js
@@ -28,12 +28,12 @@ async function main () {
   await Promise.all([
     upload(
       s3,
-      'dist/badge.bundle.js',
+      'dist/script/badge.bundle.js',
       'badge/scite-badge-latest.min.js'
     ),
     upload(
       s3,
-      'dist/badge.bundle.js',
+      'dist/script.badge.bundle.js',
       `badge/scite-badge-${VERSION}.min.js`
     )
   ])
